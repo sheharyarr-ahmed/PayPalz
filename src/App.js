fe9bc39c0e021a7caf7405aa4376de0241444225
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-09-21 08:17:11 +0500

    building the static version and implementing the css classes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,63 @@
+import "./index.css";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img>{friend.img}</img>
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          YOU OWE {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} OWES YOU {Math.abs(friend.balance)} $
+        </p>
+      )}
+      {friend.balance === 0 && <p>YOU AND {friend.name} ARE EVEN.</p>}
+      <button className="button">SELECT</button>
+    </li>
+  );
 }
